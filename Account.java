7841e7256a5ca51459f796600e341b80b241924c
kavyaemireddy <emireddykavyareddy@gmail.com>
2026-06-04 10:17:01 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,28 @@
+public class Account {
+
+    private String userId;
+    private String userPin;
+    private double balance;
+
+    public Account(String userId, String userPin, double balance) {
+        this.userId = userId;
+        this.userPin = userPin;
+        this.balance = balance;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPin() {
+        return userPin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
